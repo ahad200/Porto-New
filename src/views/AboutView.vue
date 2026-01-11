c3af6bcff2ed8f8a4baf5738496f2600ae091b35
ahad200 <diyas189@gmail.com>
2026-01-11 18:13:02 +0700

    init renew AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -134,18 +134,12 @@ export default {
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Muhammad Ahadiyas Hatumena. I'm a web developer from South Jakarta. I
-            have an experiences in Full-stack developer and Software Engineering.
+            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Muhammad Ahadiyas Hatumena. I'm a Software Engineer from South Jakarta. 
+            I strongly interest in development, distributed systems and applying analytical skills to solve complex problems. have An experience in Full-Stack Developer.
             I really enjoy what I do right now, in my opinion, creating programs is not just a job, but also an art that
-            has
-            aesthetic value
-          </p>
-          <p class="mb-3 fadein-left fadeins-2">
-            &nbsp; &nbsp; &nbsp; My job is to build your website to be functional and user-friendly yet still attractive.
-            In addition, I provide a personal touch to your product and ensure that the website catches attention and is
-            easy to use.
-            My goal is to convey your message and identity in the most creative way. If you are interested in hiring me,
-            please contact the listed contact.
+            has aesthetic value. Job is to build your website to be functional and user-friendly.
+            The goals is to convey your message and identity in the most creative way.
+
           </p>
         </div>
 
